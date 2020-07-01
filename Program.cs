d2fe8769b484d8f5212055b46ddd86483ceaf8c8
cesaraugustob18 <cesaraugustob18@outlook.com>
2020-06-30 19:06:27 -0500

    Cambio Conflicto

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace WebMartes
             CreateHostBuilder(args).Build().Run();
         }
 
+        //Cambios Sprint2
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
